60651cc19ae6952b81749676a65d9c4077daf282
CamilaCabrera <camilacabrera464@gmail.com>
2023-07-09 11:41:34 -0500

    Funciona Dijiskstra

---
+++ src/Grafo/Arista.java
@@ -0,0 +1,25 @@
+package Grafo;
+
+public class Arista {
+    private Vertice verticeInicial;
+    private Vertice verticeFinal;
+    private int peso;
+
+    public Arista(Vertice verticeInicial, Vertice verticeFinal, int peso) {
+        this.verticeInicial = verticeInicial;
+        this.verticeFinal = verticeFinal;
+        this.peso = peso;
+    }
+
+    public Vertice getVerticeInicial() {
+        return verticeInicial;
+    }
+
+    public Vertice getVerticeFinal() {
+        return verticeFinal;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+}
---
+++ src/Interfaz/Ruta.java
@@ -0,0 +1,11 @@
+package Interfaz;
+
+import javax.swing.*;
+
+public class Ruta {
+    private JTextField txtOrigen;
+    private JTextField txtDestino;
+    private JComboBox cmbHoras;
+    private JComboBox cmbMinutos;
+    private JButton btnIngresarRuta;
+}
